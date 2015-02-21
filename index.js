7ecef6c3b56017bb392285dcb85884fd43f28995
Raine Virta <raine.virta@gmail.com>
2015-02-21 03:35:50 +0200

    resolve app cwds to dir that yaml files are read from

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var spawn = require('child_process').spawn;
 var argv = require('minimist')(process.argv.slice(2));
 var readDirYAML = require('./lib/read-dir-yaml');
 var spawnPM2 = require('./lib/spawn-pm2');
+var path = require('path');
 
 var appsPath = argv._[0];
 var pm2cmd   = argv._[1];
@@ -13,8 +14,14 @@ if (argv._.length < 2) {
   process.exit(1);
 }
 
+var resolveAppCwd = curry(function(relativeDir, app) {
+  return merge(app, {
+    cwd: path.resolve(relativeDir, app.cwd)
+  });
+});
+
 Promise.coroutine(function*() {
-  var apps = yield readDirYAML(appsPath);
+  var apps = yield readDirYAML(appsPath).map(resolveAppCwd(appsPath));
   var pm2 = spawnPM2(pm2cmd, JSON.stringify(apps));
   pm2.on('exit', process.exit);
 })();
